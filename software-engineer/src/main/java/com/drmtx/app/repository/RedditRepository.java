294fee0a05308927c22394e03e2311e1a6a60059
Bernhard Woditschka <bernhard@woditschka.com>
2015-08-11 21:07:36 +0200

    added User-Agent to reddit api call

--- software-engineer/src/main/java/com/drmtx/app/repository/RedditRepository.java
+++ software-engineer/src/main/java/com/drmtx/app/repository/RedditRepository.java
@@ -2,8 +2,16 @@ package com.drmtx.app.repository;
 
 import com.drmtx.app.domain.reddit.RedditComment;
 import com.drmtx.app.domain.reddit.RedditCommentNode;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Repository;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
+
+import static org.springframework.http.HttpMethod.GET;
 
 /**
  * Repository to access the reddit comments endpoint.
@@ -18,7 +26,14 @@ public class RedditRepository {
      * @return reddis comments
      */
     public RedditComment findCommentByUrl(String url) {
-        RedditCommentNode[] redditCommentNodes = new RestTemplate().getForObject(url, RedditCommentNode[].class);
-        return new RedditComment(redditCommentNodes);
+        // add user agent to allow higher api troughput
+        HttpHeaders requestHeaders = new HttpHeaders();
+        requestHeaders.set("User-Agent", "android:com.drmtx:v1.0 (by bw)");
+
+        // query api
+        HttpEntity<MultiValueMap<String, String>> requestEntity = new HttpEntity<>(new LinkedMultiValueMap<>(), requestHeaders);
+        ResponseEntity<RedditCommentNode[]> response = new RestTemplate().exchange(url, GET, requestEntity, RedditCommentNode[].class);
+
+        return new RedditComment(response.getBody());
     }
 }
